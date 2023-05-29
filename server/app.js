313df4beb12fcb7d24f82a883c1d9a558012972e
ChakshuVerma <vermachakshu@gmail.com>
2023-05-29 11:44:14 +0530

    Removed CORS error

--- server/app.js
+++ server/app.js
@@ -8,9 +8,15 @@ const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload')
 const cors = require('cors');
 
-app.use(cors({
-    origin: '*'
-}));
+// app.use(cors({
+//     origin: '*'
+// }));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ })
 
 dotenv.config({path: './config.env'});
 
