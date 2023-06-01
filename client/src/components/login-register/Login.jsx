d64abe6fba9ab72b41c4c99c3eb308d76ac61445
ChakshuVerma <vermachakshu@gmail.com>
2023-06-01 10:17:53 +0530

    Debuggg

--- client/src/components/login-register/Login.jsx
+++ client/src/components/login-register/Login.jsx
@@ -44,6 +44,7 @@ const Login = () =>{
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
+                "credentials": "include"
             },
             body: JSON.stringify({email, password})
         });
